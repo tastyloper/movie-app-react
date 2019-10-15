c356c5316bce7b4634f4f8f91ced61475779488d
tastyloper <estprocho@gmail.com>
2019-10-15 15:03:47 +0900

    Updates

---
+++ precache-manifest.aadfaaa5861a7d8209656538298eb992.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed434a95402981f80cbf7089ea18403d",
+    "url": "/movie-app-react/index.html"
+  },
+  {
+    "revision": "c2195b06c4412f4c1d5c",
+    "url": "/movie-app-react/static/css/main.610837b0.chunk.css"
+  },
+  {
+    "revision": "273c2ea8e6baa9db7134",
+    "url": "/movie-app-react/static/js/2.e2471e63.chunk.js"
+  },
+  {
+    "revision": "c2195b06c4412f4c1d5c",
+    "url": "/movie-app-react/static/js/main.f44459ab.chunk.js"
+  },
+  {
+    "revision": "bf5364ff0cdacd8acb53",
+    "url": "/movie-app-react/static/js/runtime-main.920a8ff5.js"
+  }
+]);
